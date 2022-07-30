d8bb3356faa85db74212274da5527e9ea1cc1c37
Filip Wojnicki <filip.wojnicki@op.pl>
2022-07-30 16:04:42 +0200

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 import { AppModule } from './App/app.module';
@@ -8,6 +8,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+
+  if (process.env.NODE_ENV === 'development') {
+    const options = new DocumentBuilder()
+      .setTitle('recruitment-task-v3')
+      .setDescription('recruitment-task-v3 API description')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(3000);
 }
