ed2f1674afccf5f3d91b23ad9700ca798074d15f
Filip Wojnicki <filip.wojnicki@op.pl>
2022-07-31 11:44:10 +0200

    add request.ip package for better ip recognize

--- src/common/throttler-behind-proxy.guard.ts
+++ src/common/throttler-behind-proxy.guard.ts
@@ -1,10 +1,11 @@
 import { ThrottlerGuard } from '@nestjs/throttler';
 import { Injectable } from '@nestjs/common';
 
+import { getClientIp } from 'request-ip';
+
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
   protected getTracker(req: Record<string, any>): string {
-    console.log(req.ips.length ? req.ips[0] : req.ip);
-    return req.ips.length ? req.ips[0] : req.ip;
+    return getClientIp(req);
   }
 }
