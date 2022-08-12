6418795c7f971d200d7e294ce96e20131f55950e
Filip Wojnicki <filip.wojnicki@op.pl>
2022-08-12 08:22:04 +0200

    transform duration prop to number in moviequerydto

--- backend/src/movie/dto/movie-query.dto.ts
+++ backend/src/movie/dto/movie-query.dto.ts
@@ -1,15 +1,21 @@
 import { Transform } from 'class-transformer';
-import {
-  IsBoolean,
-  IsDate,
-  IsNumber,
-  IsNumberString,
-  IsOptional,
-} from 'class-validator';
+import { IsInt, IsOptional } from 'class-validator';
+
+function toPositiveNumber(value: string): number {
+  let newValue: number = Number.parseInt(value, 10);
+
+  if (Number.isNaN(newValue)) {
+    newValue = 0;
+  }
+
+  return Math.abs(newValue);
+}
 
 export class MovieQueryDto {
+  @Transform(({ value }) => toPositiveNumber(value))
   @IsOptional()
-  public duration: string;
+  @IsInt()
+  public duration: number = 0;
 
   @IsOptional()
   public genres: string[];
