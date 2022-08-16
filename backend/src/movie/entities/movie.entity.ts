6f567b5700076eaa86eae466ba0ec19681ade643
Filip Wojnicki <filip.wojnicki@op.pl>
2022-08-16 02:04:46 +0200

    add api properties to movie entity schema

--- backend/src/movie/entities/movie.entity.ts
+++ backend/src/movie/entities/movie.entity.ts
@@ -1,15 +1,55 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Entity, Schema } from 'redis-om';
 import { Movie } from '../interface/movie.interface';
 
 export class MovieEntity extends Entity implements Movie {
+  @ApiProperty({
+    description: 'Id of the movie',
+  })
   id: number;
+
+  @ApiProperty({
+    description: 'Title of the movie',
+  })
   title: string;
+
+  @ApiProperty({
+    description: 'Year of the movie premier',
+  })
   year: number;
+
+  @ApiProperty({
+    description: 'Duration of the movie in minutes',
+  })
   runtime: number;
+
+  @ApiProperty({
+    description: 'Director of the movie',
+  })
   director: string;
+
+  @ApiProperty({
+    description: 'Actors of the movie',
+  })
+  @ApiPropertyOptional()
   actors?: string;
+
+  @ApiProperty({
+    description: 'Plot of the movie',
+  })
+  @ApiPropertyOptional()
   plot?: string;
+
+  @ApiProperty({
+    description: 'Link to poster of the movie',
+  })
+  @ApiPropertyOptional()
   posterUrl?: string;
+
+  @ApiProperty({
+    description: 'Array with the movie genres',
+  })
+  @ApiPropertyOptional()
   genres?: string[];
 }
 
